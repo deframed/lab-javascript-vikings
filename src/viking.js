b3903ca529ef0ead6e9dcd19848212120dc66963
Delia Drel <d.drel@hotmail.com>
2021-01-18 19:37:49 +0100

    stuck at bonus iteration

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,108 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength
+    }
+
+    attack() {
+       return this.strength 
+    }
+
+    receiveDamage(damage) {
+        this.health = this.health - damage
+     }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name
+    }
+    attack() {
+        return this.strength
+    }
 
-// Saxon
-class Saxon {}
+    receiveDamage(damage) {
+            this.health = this.health - damage
 
-// War
-class War {}
+            if (this.health > 0) {
+                return `${this.name} has received ${damage} points of damage`
+            }
+            else {
+                return `${this.name} has died in act of combat` 
+         }  
+     }
+
+     battleCry() {
+        return "Odin Owns You All!"
+    }
+
+}
+
+// // Saxon
+class Saxon extends Soldier {
+    // constructor(health, strength) {
+    //     super(health, strength);
+    // }
+
+    attack() {
+        return this.strength
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage
+
+        if (this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`
+        }
+        else {
+            return `A Saxon has died in combat` 
+     }  
+    }
+}
+
+// // War
+class War extends Soldier {
+    constructor() {
+        super()
+        this.vikingArmy = [ ];
+        this.saxonArmy = [ ]
+    }
+   
+
+     addViking(vikingObj) {
+        this.vikingArmy.push(vikingObj)
+             
+   }
+
+   addSaxon(saxonObj) {
+       this.saxonArmy.push(saxonObj)
+        
+   }
+
+   vikingAttack() {
+       if (saxonObj.receiveDamage() === vikingObj.strength) {
+           this.saxonArmy -= saxonObj
+           return `${saxonObj.receiveDamage()}, ${vikingObj.strength}`
+       }
+
+   }
+
+   saxonAttack() {
+
+   }
+
+   showStatus() {
+
+   }
+
+
+
+    
+      
+    
+    
+
+}
